b221a4606526e3c0a486c0f52357a84ca8f2b0b8
edrianypuji <edrianypuji@hotmail.com>
2019-07-09 01:27:40 +0200

    Updates

---
+++ precache-manifest.f4dc8ad4efbf591d5979a634ec475096.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75bf97db8734da156b5b56f58e264a5d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4937914e859e15bedc27",
+    "url": "/monsters-rolodex/static/css/main.19385adf.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4937914e859e15bedc27",
+    "url": "/monsters-rolodex/static/js/main.15734624.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
